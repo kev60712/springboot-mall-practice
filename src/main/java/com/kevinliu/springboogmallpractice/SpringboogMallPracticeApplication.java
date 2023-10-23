651d8b58693b6ae747243f1501c0214e6e6e37ff
Kevin Liu <kev60712@gmail.com>
2023-10-23 17:13:27 -0400

    init spring boot project

---
+++ src/main/java/com/kevinliu/springboogmallpractice/SpringboogMallPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.kevinliu.springboogmallpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboogMallPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboogMallPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kevinliu/springboogmallpractice/SpringboogMallPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinliu.springboogmallpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboogMallPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
